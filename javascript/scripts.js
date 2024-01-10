48bcc2a337c3d8d756899cf33e8b85f7525f44e5
danieleangeletti <angelettidaniele@gmail.com>
2024-01-10 22:36:16 +0000

    aggiustamento coordinate x e y

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -28,7 +28,7 @@ for (let i = 0; i < board.length; i++) {
 
         const my_box = document.createElement("div");
         my_box.classList.add("square");
-        my_box.setAttribute("id", [7 - i, j]);
+        my_box.setAttribute("id", [j, i]);
 
         if ((i + j) % 2 != 0) {
             my_box.classList.add("bg-dark");
@@ -46,80 +46,80 @@ for (let i = 0; i < board.length; i++) {
 
 // AGGIUNTA PEZZI
 
+document.getElementById("0,0").innerHTML = `<img class="w-100 h-100" src="./img/red-rook.png" alt=""></img>`;
+board[0][0].piece = "rook";
+board[0][0].player = 1;
+
 document.getElementById("7,0").innerHTML = `<img class="w-100 h-100" src="./img/red-rook.png" alt=""></img>`;
 board[7][0].piece = "rook";
 board[7][0].player = 1;
-
-document.getElementById("7,7").innerHTML = `<img class="w-100 h-100" src="./img/red-rook.png" alt=""></img>`;
-board[7][7].piece = "rook";
-board[7][7].player = 1;
-
-document.getElementById("0,0").innerHTML = `<img class="w-100 h-100" src="./img/yellow-rook.png" alt=""></img>`;
-board[0][0].piece = "rook";
-board[0][0].player = 0;
 
 document.getElementById("0,7").innerHTML = `<img class="w-100 h-100" src="./img/yellow-rook.png" alt=""></img>`;
 board[0][7].piece = "rook";
 board[0][7].player = 0;
 
-document.getElementById("7,1").innerHTML = `<img class="w-100 h-100" src="./img/red-knight.png" alt=""></img>`;
-board[7][1].piece = "knight";
-board[7][1].player = 1;
+document.getElementById("7,7").innerHTML = `<img class="w-100 h-100" src="./img/yellow-rook.png" alt=""></img>`;
+board[7][7].piece = "rook";
+board[7][7].player = 0;
 
-document.getElementById("7,6").innerHTML = `<img class="w-100 h-100" src="./img/red-knight.png" alt=""></img>`;
-board[7][6].piece = "knight";
-board[7][6].player = 1;
+document.getElementById("1,0").innerHTML = `<img class="w-100 h-100" src="./img/red-knight.png" alt=""></img>`;
+board[1][0].piece = "knight";
+board[1][0].player = 1;
 
-document.getElementById("0,1").innerHTML = `<img class="w-100 h-100" src="./img/yellow-knight.png" alt=""></img>`;
-board[0][1].piece = "knight";
-board[0][1].player = 0;
+document.getElementById("6,0").innerHTML = `<img class="w-100 h-100" src="./img/red-knight.png" alt=""></img>`;
+board[6][0].piece = "knight";
+board[6][0].player = 1;
 
-document.getElementById("0,6").innerHTML = `<img class="w-100 h-100" src="./img/yellow-knight.png" alt=""></img>`;
-board[0][6].piece = "knight";
-board[0][6].player = 0;
+document.getElementById("1,7").innerHTML = `<img class="w-100 h-100" src="./img/yellow-knight.png" alt=""></img>`;
+board[1][7].piece = "knight";
+board[1][7].player = 0;
 
-document.getElementById("7,2").innerHTML = `<img class="w-100 h-100" src="./img/red-bishop.png" alt=""></img>`;
-board[7][2].piece = "bishop";
-board[7][2].player = 1;
+document.getElementById("6,7").innerHTML = `<img class="w-100 h-100" src="./img/yellow-knight.png" alt=""></img>`;
+board[6][7].piece = "knight";
+board[6][7].player = 0;
 
-document.getElementById("7,5").innerHTML = `<img class="w-100 h-100" src="./img/red-bishop.png" alt=""></img>`;
-board[7][5].piece = "bishop";
-board[7][5].player = 1;
+document.getElementById("2,0").innerHTML = `<img class="w-100 h-100" src="./img/red-bishop.png" alt=""></img>`;
+board[2][0].piece = "bishop";
+board[2][0].player = 1;
 
-document.getElementById("0,2").innerHTML = `<img class="w-100 h-100" src="./img/yellow-bishop.png" alt=""></img>`;
-board[0][2].piece = "bishop";
-board[0][2].player = 0;
+document.getElementById("5,0").innerHTML = `<img class="w-100 h-100" src="./img/red-bishop.png" alt=""></img>`;
+board[5][0].piece = "bishop";
+board[5][0].player = 1;
 
-document.getElementById("0,5").innerHTML = `<img class="w-100 h-100" src="./img/yellow-bishop.png" alt=""></img>`;
-board[0][5].piece = "bishop";
-board[0][5].player = 0;
+document.getElementById("2,7").innerHTML = `<img class="w-100 h-100" src="./img/yellow-bishop.png" alt=""></img>`;
+board[2][7].piece = "bishop";
+board[2][7].player = 0;
 
-document.getElementById("7,3").innerHTML = `<img class="w-100 h-100" src="./img/red-queen.png" alt=""></img>`;
-board[7][3].piece = "queen";
-board[7][3].player = 1;
+document.getElementById("5,7").innerHTML = `<img class="w-100 h-100" src="./img/yellow-bishop.png" alt=""></img>`;
+board[5][7].piece = "bishop";
+board[5][7].player = 0;
 
-document.getElementById("7,4").innerHTML = `<img class="w-100 h-100" src="./img/red-king.png" alt=""></img>`;
-board[7][4].piece = "king";
-board[7][4].player = 1;
+document.getElementById("3,0").innerHTML = `<img class="w-100 h-100" src="./img/red-queen.png" alt=""></img>`;
+board[3][0].piece = "queen";
+board[3][0].player = 1;
 
-document.getElementById("0,3").innerHTML = `<img class="w-100 h-100" src="./img/yellow-queen.png" alt=""></img>`;
-board[0][3].piece = "queen";
-board[0][3].player = 0;
+document.getElementById("4,0").innerHTML = `<img class="w-100 h-100" src="./img/red-king.png" alt=""></img>`;
+board[4][0].piece = "king";
+board[4][0].player = 1;
 
-document.getElementById("0,4").innerHTML = `<img class="w-100 h-100" src="./img/yellow-king.png" alt=""></img>`;
-board[0][4].piece = "king";
-board[0][4].player = 0;
+document.getElementById("3,7").innerHTML = `<img class="w-100 h-100" src="./img/yellow-queen.png" alt=""></img>`;
+board[3][7].piece = "queen";
+board[3][7].player = 0;
+
+document.getElementById("4,7").innerHTML = `<img class="w-100 h-100" src="./img/yellow-king.png" alt=""></img>`;
+board[4][7].piece = "king";
+board[4][7].player = 0;
 
 for (let i = 0; i < board[0].length; i++) {
-    document.getElementById(`6,${i}`).innerHTML = `<img class="w-100 h-100" src="./img/red-pawn.png" alt=""></img>`;
-    board[6][i].piece = "pawn";
-    board[6][i].player = 1;
+    document.getElementById(`${i},1`).innerHTML = `<img class="w-100 h-100" src="./img/red-pawn.png" alt=""></img>`;
+    board[i][1].piece = "pawn";
+    board[i][1].player = 1;
 }
 
 for (let i = 0; i < board[0].length; i++) {
-    document.getElementById(`1,${i}`).innerHTML = `<img class="w-100 h-100" src="./img/yellow-pawn.png" alt=""></img>`;
-    board[1][i].piece = "pawn";
-    board[1][i].player = 0;
+    document.getElementById(`${i},6`).innerHTML = `<img class="w-100 h-100" src="./img/yellow-pawn.png" alt=""></img>`;
+    board[i][6].piece = "pawn";
+    board[i][6].player = 0;
 }
 
 // GESTIONE CLICK
@@ -133,14 +133,14 @@ for (let i = 0; i < board.length; i++) {
 
     for (let j = 0; j < board[i].length; j++) {
 
-        const my_box = document.getElementById(`${i},${j}`);
+        const my_box = document.getElementById(`${j},${i}`);
 
         my_box.addEventListener("click", function() {
 
-            clicked_box = board[i][j];
+            clicked_box = board[j][i];
 
             if ((active == false) && (clicked_box.player == 0) && (turn == 0)) {
-                active_box = board[i][j];
+                active_box = board[j][i];
                 active = true;
                 console.log("Attivando: ", active_box);
             }
@@ -174,7 +174,7 @@ function move(box, destination) {
     switch (box.piece) {
         case "pawn":
             // MOVE PAWN
-            if ((destination.x == box.x + 1) && (box.y == destination.y)) {
+            if ((destination.x == box.x) && (destination.y == box.y - 1)) {
                 board[destination.x][destination.y] = {
                     x: destination.x,
                     y: destination.y,
